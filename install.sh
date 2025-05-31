d2188e7e82debf4d2d8e556132059b3c0f00e19e
Carlos Rubio <carlosreyniery@gmail.com>
2025-05-31 16:01:28 -0600

    Fixing install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ln -sf ./dotfiles/nvim/nvim ~/.config/nvim
-ln -sf ./dotfiles/starship/starship.toml ~/.config/starship.toml
-ln -sf ./dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -sf ./dotfiles/zsh/zshrc ~/.zshrc
+cp ./nvim ~/.config/nvim
+cp ./starship/starship.toml ~/.config/starship.toml
+ln ./tmux/.tmux.conf ~/.tmux.conf
+ln ./zsh/.zshrc ~/.zshrc
 
 echo "Installed dotfiles"
